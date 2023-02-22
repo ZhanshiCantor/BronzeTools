2ee103e39621af056d853d6c626d5ba7751d2df3
simon <simon.riehl.2001@gmail.com>
2023-02-22 03:09:46 +0100

    setup 11.45

---
+++ src/main/java/com/zhanshicantor/bronzetools/BronzeTools.java
@@ -0,0 +1,10 @@
+package com.zhanshicantor.bronzetools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BronzeTools implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
