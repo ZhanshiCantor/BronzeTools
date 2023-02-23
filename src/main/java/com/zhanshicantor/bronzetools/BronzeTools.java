15dd6491a76d58f8fa91cd293c415a611eea0406
simon <simon.riehl.2001@gmail.com>
2023-02-23 22:20:19 +0100

    project setup complete

--- src/main/java/com/zhanshicantor/bronzetools/BronzeTools.java
+++ src/main/java/com/zhanshicantor/bronzetools/BronzeTools.java
@@ -3,6 +3,8 @@ package com.zhanshicantor.bronzetools;
 import net.fabricmc.api.ModInitializer;
 
 public class BronzeTools implements ModInitializer {
+
+    public static final String MOD_ID = "bronze";
     @Override
     public void onInitialize() {
 
